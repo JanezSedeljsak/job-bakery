22b2169b6930b9fb00f81992e759fa45c2a06036
JanezSedeljsak <43420276+JanezSedeljsak@users.noreply.github.com>
2019-12-04 09:24:52 +0100

    added 2 new scaffolds

---
+++ app/views/areas/_area.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! area, :id, :title, :created_at, :updated_at
+json.url area_url(area, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191126112834) do
+ActiveRecord::Schema.define(version: 20191204080108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "areas", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "commontator_comments", force: :cascade do |t|
-    t.bigint "thread_id", null: false
+    t.integer "thread_id", null: false
     t.string "creator_type", null: false
-    t.bigint "creator_id", null: false
+    t.integer "creator_id", null: false
     t.string "editor_type"
-    t.bigint "editor_id"
+    t.integer "editor_id"
     t.text "body", null: false
     t.datetime "deleted_at"
     t.integer "cached_votes_up", default: 0
     t.integer "cached_votes_down", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "parent_id"
+    t.integer "parent_id"
     t.index ["cached_votes_down"], name: "index_commontator_comments_on_cached_votes_down"
     t.index ["cached_votes_up"], name: "index_commontator_comments_on_cached_votes_up"
     t.index ["creator_id", "creator_type", "thread_id"], name: "index_commontator_comments_on_c_id_and_c_type_and_t_id"
@@ -37,9 +43,9 @@ ActiveRecord::Schema.define(version: 20191126112834) do
   end
 
   create_table "commontator_subscriptions", force: :cascade do |t|
-    t.bigint "thread_id", null: false
+    t.integer "thread_id", null: false
     t.string "subscriber_type", null: false
-    t.bigint "subscriber_id", null: false
+    t.integer "subscriber_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["subscriber_id", "subscriber_type", "thread_id"], name: "index_commontator_subscriptions_on_s_id_and_s_type_and_t_id", unique: true
@@ -48,9 +54,9 @@ ActiveRecord::Schema.define(version: 20191126112834) do
 
   create_table "commontator_threads", force: :cascade do |t|
     t.string "commontable_type"
-    t.bigint "commontable_id"
+    t.integer "commontable_id"
     t.string "closer_type"
-    t.bigint "closer_id"
+    t.integer "closer_id"
     t.datetime "closed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -67,12 +73,19 @@ ActiveRecord::Schema.define(version: 20191126112834) do
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.string "title"
+    t.string "postcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.integer "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -89,9 +102,5 @@ ActiveRecord::Schema.define(version: 20191126112834) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "commontator_comments", "commontator_comments", column: "parent_id", on_update: :restrict, on_delete: :cascade
-  add_foreign_key "commontator_comments", "commontator_threads", column: "thread_id", on_update: :cascade, on_delete: :cascade
-  add_foreign_key "commontator_subscriptions", "commontator_threads", column: "thread_id", on_update: :cascade, on_delete: :cascade
   add_foreign_key "jobs", "users"
-  add_foreign_key "posts", "users"
 end
