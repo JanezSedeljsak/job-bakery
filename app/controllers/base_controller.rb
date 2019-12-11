feae9deca9317ded991c4af9d16446e19232b285
JanezSedeljsak <43420276+JanezSedeljsak@users.noreply.github.com>
2019-12-11 09:36:53 +0100

    split normal users from companies & new views

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,6 +1,18 @@
 class BaseController < ApplicationController
     def index
-        @locations = Location.all
-        @areas = Area.all
+        if user_signed_in?
+            if(current_user.company?)
+                @jobs = Job.all
+                render :template => "base/_company"
+            else
+                @locations = Location.all
+                @areas = Area.all
+                render :template => "base/_noncompany"
+            end
+        else
+            @locations = Location.all
+            @areas = Area.all
+            render :template => "base/_noncompany"
+        end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191210122112) do
+ActiveRecord::Schema.define(version: 20191211072115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 20191210122112) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.boolean "isCompany"
+    t.boolean "company", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
