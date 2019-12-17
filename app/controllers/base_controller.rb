b18fe3c0e97553f451536e0e509fa2c8973534a4
JanezSedeljsak <43420276+JanezSedeljsak@users.noreply.github.com>
2019-12-17 12:27:34 +0100

    jobs view conditions

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,7 +2,7 @@ class BaseController < ApplicationController
     def index
         if user_signed_in?
             if(current_user.company?)
-                @jobs = Job.all
+                @jobs = Job.where({ user: current_user })
                 render :template => "base/_company"
             else
                 @locations = Location.all
